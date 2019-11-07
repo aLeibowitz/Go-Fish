7194fdda60cd2e75e93982ade1a66c8265b17b10
ViviL <47183724+aLeibowitz@users.noreply.github.com>
2019-11-07 13:38:19 +0200

    Computer Player's Turn + made it keep track of currentPlayer
    for Polymorphism -- can use AskForCard(), etc. methods, for both players now!

--- GoFish-VL/Game.cs
+++ GoFish-VL/Game.cs
@@ -15,6 +15,7 @@ namespace GoFish_VL
         public int pl1NumOfBooks = 0;
         public int compNumOfBooks = 0;
         public Deck deck;
+        public string currentPlayer;
         
 
         public Game(Deck deck)
@@ -24,9 +25,31 @@ namespace GoFish_VL
 
         public string AskForCard()
         {
-            Console.WriteLine("Type in the rank of the card you want to ask me for: ");
-            string cardWanted = Console.ReadLine();
-            return cardWanted;
+            string cardRequested = null;
+
+            if (currentPlayer == "Human")
+            {
+                Console.WriteLine("Type in the rank of the card you want to ask me for: ");
+                cardRequested = Console.ReadLine();
+                return cardRequested;
+            }
+            else 
+            {
+                ArrayList alreadyRequested = new ArrayList();
+
+                Random random = new Random();
+                int ixCompCards = random.Next(0, deck.compCards.Count);
+
+                Cards cardRequested1 = (Cards)deck.compCards[ixCompCards];
+                cardRequested = cardRequested1._rank; //turns it into a string
+
+                if (alreadyRequested.Contains(cardRequested))
+                    AskForCard();
+
+                else alreadyRequested.Add(cardRequested);
+                
+                return cardRequested;
+            }
         }
 
         public bool CheckIfHas(ArrayList deck, string cardWanted)
@@ -94,28 +117,47 @@ namespace GoFish_VL
 
                 if (numOfRank == 4)
                 {
-                    Console.WriteLine("Great job! You got 4 cards of the same rank!");
+
+                    if (currentPlayer == "Human")
+                        Console.WriteLine($"Great job! You completed the book of {rank}'s!");
+                    
+                    else if (currentPlayer == "Computer")
+                        Console.WriteLine($"Good for me! I completed the book of {rank}'s!");
+
                     foreach (Cards card in cardsOfSameRank)
                     {
-                        booksPile.Add(cardsOfSameRank);
-                        deck.Remove(cardsOfSameRank);
+                        booksPile.Add(card);  //did this first here and below: cardsOfSameRank
+                        deck.Remove(card);
                     }
                     numOfBooks++;
+
+                    if (currentPlayer == "Human")
+                        Console.WriteLine($"You have {pl1NumOfBooks} books so far! Keep it up!!!");
+
+                    else if (currentPlayer == "Computer")
+                        Console.WriteLine($"I have {compNumOfBooks} so far.");
+
                 }
             }
+            currentPlayer = null;
 
         }
 
         public void HumanPlayerTurn()
         {
+            
+
+            //first see if he made a match -- instead of writing it after picking and after getting a card, can do it here, cuz it makes them go again, anyways.
+            CheckForBooks(deck.pl1Cards, pl1Books, pl1NumOfBooks);
+
             //show the player his cards
             Console.WriteLine("Here are your cards: ");
             deck.PrintDeck(deck.pl1Cards);
 
             //show the player how many books he won
-            Console.WriteLine("Number of books you already won: {0}", pl1NumOfBooks);
-            Console.WriteLine();
-
+            //Console.WriteLine("Number of books you already won: {0}", pl1NumOfBooks);
+            //Console.WriteLine();
+            currentPlayer = "Human";
             string cardRequested = AskForCard();
             bool validRequest = CheckIfHas(deck.pl1Cards, cardRequested);
             Console.WriteLine();
@@ -130,8 +172,9 @@ namespace GoFish_VL
                 bool success = CheckIfHas(deck.compCards, cardRequested); //for computer's turn, jsut put in the player's deck as the 1st parameter.
                 if (success)
                 {
-                    Console.WriteLine("Good for you! I have it!");
-                    Console.WriteLine("You get to go again.");
+                    Console.WriteLine("I have it! Here you go.");
+                    Console.WriteLine("You get to go again!");
+                    Console.WriteLine();
 
                     RemoveCards(deck.compCards, cardRequested); //1st parameter is the one giving up the cards.
                     AddThemIn(deck.pl1Cards); //this parameter is the one who asked for them.
@@ -171,6 +214,50 @@ namespace GoFish_VL
                 }
                 Console.WriteLine();
             }
+
+        }
+
+        public void ComputerPlayerTurn()
+        {
+            currentPlayer = "Computer";
+
+            CheckForBooks(deck.compCards, compBooks, compNumOfBooks);
+
+            string cardRequested = AskForCard();
+
+            Console.WriteLine($"Do you have any {cardRequested}'s?");
+
+            bool success = CheckIfHas(deck.pl1Cards, cardRequested);
+            if (success)
+            {
+                Console.WriteLine("Thanks! I get to go again!");
+                RemoveCards(deck.pl1Cards, cardRequested); //1st parameter is the one giving up the cards.
+                AddThemIn(deck.compCards); //this parameter is the one who asked for them.
+
+                CheckForBooks(deck.compCards, compBooks, compNumOfBooks); //1st parameter:whose turn it is. 2nd parameter: his book pile. 3rd: his amount of books.
+
+                ComputerPlayerTurn();
+            }
+            else
+            {
+                Console.WriteLine("Nope? Ok, I'll go fish.");
+
+                deck.compCards.Add(deck.centerPile[0]);
+                Cards cardPicked = (Cards)deck.centerPile[0];
+
+                deck.centerPile.Remove(deck.centerPile[0]);
+
+                if (cardPicked._rank == cardRequested)
+                {
+                    Console.WriteLine("I picked the card I asked for! I get to go again!");
+                    ComputerPlayerTurn();
+                }
+            }
+            Console.WriteLine();
+            
+
+
+
 
         }
     }
--- GoFish-VL/Program.cs
+++ GoFish-VL/Program.cs
@@ -77,8 +77,21 @@ namespace GoFish_VL
                 Console.WriteLine("These are my cards: ");
                 deck.PrintDeck(deck.compCards);
 
+                game.ComputerPlayerTurn();
             }
-			while (deck.centerPile.Count > 0); //not correct. change it to when all 13 books have been won. i.e. while (compbookswon + humanbookswon <13)
+            //while (deck.centerPile.Count > 0); //not correct. change it to when all 13 books have been won. i.e. while (compbookswon + humanbookswon <13)
+            while (game.pl1NumOfBooks + game.compNumOfBooks < 13);
+
+            Console.WriteLine($"You won {game.pl1NumOfBooks} books, and I won {game.compNumOfBooks} books.");
+
+            if(game.pl1NumOfBooks > game.compNumOfBooks)
+            {
+                Console.WriteLine("YOU WIN!!!!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, better luck next time!");
+            }
 
 			Console.ReadKey();
 		}
